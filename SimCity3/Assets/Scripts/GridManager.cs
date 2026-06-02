2acf130028fcaa27a85cbec65ec364fcb9e4156b
khongpak <74703923+khongpak@users.noreply.github.com>
2026-06-02 12:56:51 +0700

    เร่ิมทำ หาตำแหน่ง เมาส์ screenpoint worldpoint

--- SimCity3/Assets/Scripts/GridManager.cs
+++ SimCity3/Assets/Scripts/GridManager.cs
@@ -1,32 +1,40 @@
 using UnityEngine;
-using TMPro; 
+using TMPro;
+using Unity.Collections;
+using UnityEngine.InputSystem;
 
 public class GridManager : MonoBehaviour
 {
-   public int width = 10;
-   public int height = 10;
-   public float cellSize = 1f;
-   public GameObject boxPrefab;
+    public int width = 10;
+    public int height = 10;
+    public float cellSize = 1f;
+    public GameObject boxPrefab;
 
-   private GameObject[,] gridArray;
-   private Vector2 gridOffset;
+    private Vector2 gridOffset;
+    private GameObject[,] gridArray;
 
     void Start()
     {
-        gridOffset = new Vector2(-(width/2) * cellSize + (cellSize/2), -(height/2)*cellSize + (cellSize/2));
         gridArray = new GameObject[width,height];
+        gridOffset = new Vector2(-(width/2) * cellSize +(cellSize/2),-(height/2)*cellSize+(cellSize/2));
 
-        createGrid();
+        CreateGrid();
     }
 
-    private void createGrid()
+    void Update()
+    {
+        Vector2 mouseScreenPos = Mouse.current.position.ReadValue();
+        Debug.Log(mouseScreenPos);
+    }
+
+    private void CreateGrid()
     {
         for(int x = 0; x < width; x++)
         {
-            for(int y = 0; y < height; y++)
+            for(int y = 0; y< height; y++)
             {
-                Vector3 spawnPosition = new Vector3(gridOffset.x + (x * cellSize), gridOffset.y +(y * cellSize),0);
-                GameObject visualBox = Instantiate(boxPrefab,spawnPosition,Quaternion.identity);
+                Vector3 spawnPosition = new Vector3(gridOffset.x + (x*cellSize),gridOffset.y +(y * cellSize),0);
+                GameObject visualBox = Instantiate(boxPrefab, spawnPosition, Quaternion.identity);
                 gridArray[x,y] = visualBox;
 
                 TextMeshPro textComponent = visualBox.GetComponentInChildren<TextMeshPro>();
@@ -36,5 +44,6 @@ public class GridManager : MonoBehaviour
                 }
             }
         }
+        
     }
 }
